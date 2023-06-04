8da4bed8f7692f8db3bc749af6e3a90aaae93996
samfa89 <djarounmusta1989@gmail.com>
2023-06-03 18:42:55 -0600

    unit

--- src/company_app_design/UnitTestingEmployeeInfo.java
+++ src/company_app_design/UnitTestingEmployeeInfo.java
@@ -2,11 +2,26 @@ package company_app_design;
 
 public class UnitTestingEmployeeInfo {
 
-    /**
-     * Use this class to write unit tests for all user-defined methods that you have implemented in this package.
-     */
-
     public static void main(String[] args) {
+        // Call your unit tests here
+        testEmployeeInfo();
+    }
 
+    public static void testEmployeeInfo() {
+        // Create an instance of your concrete EmployeeInfo class
+        EmployeeInfo employeeInfo = new EmployeeInfo();
+
+        // Test the methods of the EmployeeInfo class
+        int employeeId = employeeInfo.employeeId();
+        String employeeName = employeeInfo.employeeName();
+        employeeInfo.assignDepartment();
+        int salary = employeeInfo.calculateSalary();
+        employeeInfo.benefits();
+
+        // Perform assertions to verifythe expected results
+        // Assert.assertEquals(expected, actual);
+        // Example: Assert.assertEquals(10, employeeId);
+        // Example: Assert.assertEquals("John Doe", employeeName);
     }
 }
+
