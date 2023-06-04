21ccadf782c67d00703c3cf827b828b39846f4f4
samfa89 <djarounmusta1989@gmail.com>
2023-06-03 20:33:00 -0600

    anagram

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,18 +1,42 @@
 package string_problems;
 
+import java.util.Arrays;
+
 public class Anagram {
 
-    /** INSTRUCTIONS
-     * Write a Java Program, `isAnagram`, to check if any two strings are anagrams
+    /**
+     * Checks if two strings are anagrams.
      *
-     *  An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, using all of the
-     *  original letters exactly once.
-     *
-     *         Ex: "CAT" & "ACT",
-     *             "ARMY" & "MARY",
-     *             "FART" & "RAFT"
+     * @param str1 the first string
+     * @param str2 the second string
+     * @return true if the strings are anagrams, false otherwise
      */
+    public static boolean isAnagram(String str1, String str2) {
+        // Remove whitespace and convert to lowercase for case-insensitive comparison
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
 
-    // Implement Here
+        // Convert the strings to character arrays and sort them
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
 
+        // Compare the sorted character arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "CAT";
+        String str2 = "ACT";
+        System.out.println("Are '" + str1 + "' and '" + str2 + "' anagrams? " + isAnagram(str1, str2));
+
+        str1 = "ARMY";
+        str2 = "MARY";
+        System.out.println("Are '" + str1 + "' and '" + str2 + "' anagrams? " + isAnagram(str1, str2));
+
+        str1 = "FART";
+        str2 = "RAFT";
+        System.out.println("Are '" + str1 + "' and '" + str2 + "' anagrams? " + isAnagram(str1, str2));
+    }
 }
